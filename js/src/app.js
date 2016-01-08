55559cf34d72929f058d276693d8b79d4ab2b496
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-01-09 02:42:27 +0530

    Smoothscroll for register buttons

--- js/src/app.js
+++ js/src/app.js
@@ -4,25 +4,30 @@
     $('document').ready(function() {
         var smoothScroll = function(section, speed) {
             $("html, body").animate({
-                scrollTop: $("#" + section).offset().top
+                scrollTop: $(section).offset().top
             }, speed);
-            history.pushState(null, null, '#'+section);
+            history.pushState(null, null, section);
             return false;
         };
-        $('.navigation li, .mobile-nav-menu li').on('click', function() {
+        $('.navigation li, .mobile-nav-menu li, .register-btn').on('click', function() {
             var id = $(this).find('a').attr('id');
-            var section = id.replace('nav-', '').replace('-mobile', '');
-            smoothScroll(section, 600);
-            document.ontouchmove = function(e) {
-                return true;
-            };
-            $('.mobile-nav-menu').fadeOut(150);
+            if(id) {
+                var section = id.replace('nav-', '').replace('-mobile', '');
+                smoothScroll('#'+section, 600);
+                document.ontouchmove = function(e) {
+                    return true;
+                };
+                $('.mobile-nav-menu').fadeOut(150);
+            }
+            else {
+                smoothScroll($.attr(this, 'href'), 600);
+            }
             return false;
         });
         $(window).scroll(function(event) {
             var scroll = $(window).scrollTop();
             var $navigation = $('.navigation');
-            
+
             scroll > 535 ? $navigation.addClass('navigation-fixed', 200) : $navigation.removeClass('navigation-fixed', 200);
         });
         $('.mobile-nav').click(function() {
